7877bfb09bbb1efeabcac7851f15bb10acb538de
hazemhassan1411 <hazemhassan1411@gmail.com>
2023-10-17 04:14:29 -0700

    d

--- main.h
+++ main.h
@@ -7,7 +7,8 @@
 
 void print_number(int n);
 int num_printf(const char *format, ...);
-
+int parser(const char *format, char *buffer, size_t maxlength,va_list args, size_t indx);
+int _printf(const char *format, ...);
 
 
 #endif
