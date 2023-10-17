1e3bf98fb2646854c8246a08754e10436bf2c6ba
hamzaaboshady <hamza.esc.1@gmail.com>
2023-10-17 04:59:29 -0700

    main.h

--- main.h
+++ main.h
@@ -1,14 +1,8 @@
 #ifndef _MAIN_H
 #define _MAIN_H
-
 #include <stdarg.h>
 #include <unistd.h>
-#include <stdio.h>
-
-void print_number(int n);
-int num_printf(const char *format, ...);
-int parser(const char *format, char *buffer, size_t maxlength,va_list args, size_t indx);
+int string(const char *format, char *buffer, size_t maxlength,
+va_list args, size_t indx);
 int _printf(const char *format, ...);
-
-
 #endif
